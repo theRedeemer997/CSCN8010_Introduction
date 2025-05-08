e9bfce37ed6327e48f7a4b7e6f1dc79e3b2ec6f7
theRedeemer997 <manu71997mathew@gmail.com>
2025-05-08 14:21:43 -0400

    Add HelloPython class with basic hello world functionality

---
+++ Foo.py
@@ -0,0 +1,9 @@
+class HelloPython:
+    def __init__(self):
+        self.say_hello()
+
+    def say_hello(self):
+        print("hello world")
+
+if __name__ == "__main__":
+    greeter = HelloPython()
